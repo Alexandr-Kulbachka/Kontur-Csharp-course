7cf80e5518affaaab7e837c80d28006a4f49f3cd
Alexandr-Kulbachka <alexandr.kulbachka@gmail.com>
2020-03-06 00:59:44 +0300

    Three projects on the subject of error

---
+++ Theme Errors/Percent/Percent/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Percent
+{
+    class Program
+    {
+        //using System.Globalization Такое подключение библиотеки вызывает ошибку:
+        //Не нужно писать весь исходный файл целиком — пишите только метод / класс, который необходим в //задаче.
+        public static double Calculate(string inputData)
+        {
+            var inputParams = inputData.Split(' ');
+            var amountOfMoney = double.Parse(inputParams[0], System.Globalization.CultureInfo.InvariantCulture);
+            var interestRate = double.Parse(inputParams[1], System.Globalization.CultureInfo.InvariantCulture);
+            var amountOfMonth = int.Parse(inputParams[2]);
+            var persentOfMonth = interestRate / 1200;
+            return amountOfMoney * Math.Pow((1 + persentOfMonth), amountOfMonth);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate("100.00 12 1"));
+        }
+    }
+}
