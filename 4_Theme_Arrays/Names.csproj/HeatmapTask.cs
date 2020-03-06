56e4ae40f4b4a211bb7abcb050637b15ff0accc4
Alexandr-Kulbachka <alexandr.kulbachka@gmail.com>
2020-03-07 00:20:10 +0300

    add theme Arrays

---
+++ 4_Theme_Arrays/Names.csproj/HeatmapTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        const int XSize = 30;
+        const int YSize = 12;
+        public static double[,] CalculateBirthsCounts(NameData[] names)
+        {
+            var birthsCounts = new double[XSize, YSize];
+            foreach (var currentName in names)
+                if (currentName.BirthDate.Day != 1)
+                    birthsCounts[currentName.BirthDate.Day - 2, currentName.BirthDate.Month - 1]++;
+            return birthsCounts;
+        }
+
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var xLabels = new string[XSize];
+            for (var y = 0; y < xLabels.Length; y++)
+                xLabels[y] = (y + 2).ToString();
+            var yLabels = new string[YSize];
+            for (var y = 0; y < yLabels.Length; y++)
+                yLabels[y] = (y + 1).ToString();
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                CalculateBirthsCounts(names),
+                xLabels,
+                yLabels);
+        }
+    }
+}
---
+++ 4_Theme_Arrays/Names.csproj/HistogramTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var minDay = 1;
+            var maxDay = 31;
+
+            var days = new string[maxDay];
+            for (var y = 0; y < days.Length; y++)
+                days[y] = (y + minDay).ToString();
+            var birthsCounts = new double[maxDay];
+            foreach (var currentName in names)
+                if (currentName.Name == name && currentName.BirthDate.Day != 1)
+                    birthsCounts[currentName.BirthDate.Day - 1]++;
+
+            return new HistogramData(
+                $"Рождаемость людей с именем {name}",
+                days,
+                birthsCounts);
+        }
+    }
+}
