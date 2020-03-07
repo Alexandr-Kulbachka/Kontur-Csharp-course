aa1b5c3238adfea9fc54dc5507c07cf298f3b322
Alexandr-Kulbachka <alexandr.kulbachka@gmail.com>
2020-03-08 00:14:18 +0300

    Add theme basics of OOP

---
+++ 10_Theme_Basics_of_OOP/Geometry/Geometry/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryTasks
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public Vector Add(Vector secondVector)
+        {
+            return Geometry.Add(this, secondVector);
+        }
+
+        public bool Belongs(Segment segment)
+        {
+            return Geometry.IsVectorInSegment(this, segment);
+        }
+    }
+
+    public class Segment
+    {
+        public Vector Begin;
+        public Vector End;
+
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public bool Contains(Vector vector)
+        {
+            return Geometry.IsVectorInSegment(vector, this);
+        }
+    }
+
+    public class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+        }
+
+        public static double GetLength(Segment segment)
+        {
+            return Math.Sqrt((segment.End.X - segment.Begin.X) *
+                             (segment.End.X - segment.Begin.X) +
+                             (segment.End.Y - segment.Begin.Y) *
+                             (segment.End.Y - segment.Begin.Y));
+        }
+
+        public static Vector Add(Vector firstVector, Vector secondVector)
+        {
+            return new Vector() { X = firstVector.X + secondVector.X, Y = firstVector.Y + secondVector.Y };
+        }
+
+        public static bool IsVectorInSegment(Vector vector, Segment segment)
+        {
+            return Math.Abs((GetLength(new Segment() { Begin = segment.Begin, End = vector }) +
+                GetLength(new Segment() { Begin = vector, End = segment.End })) -
+                GetLength(segment)) < 0.000000001;
+        }
+    }
+}
---
+++ 10_Theme_Basics_of_OOP/GeometryPainting.csproj/SegmentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace GeometryPainting
+{
+    //Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+    public static class SegmentExtensions
+    {
+        private readonly static Dictionary<Segment, Color> allColors =
+            new Dictionary<Segment, Color>();
+
+        public static Color GetColor(this Segment segment)
+        {
+            return allColors.ContainsKey(segment) ? allColors[segment] : Color.Black;
+        }
+
+        public static void SetColor(this Segment segment, Color color)
+        {
+            allColors[segment] = color;
+        }
+    }
+}
