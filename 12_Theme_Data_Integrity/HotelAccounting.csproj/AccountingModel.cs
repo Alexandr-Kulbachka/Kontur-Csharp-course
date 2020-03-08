52d9e5944ecf367bdeabbdbe4784018f1e1b5b9b
Alexandr-Kulbachka <alexandr.kulbachka@gmail.com>
2020-03-08 18:32:44 +0300

    Add theme Data integrity

---
+++ 12_Theme_Data_Integrity/HotelAccounting.csproj/AccountingModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAccounting
+{
+    public class AccountingModel : ModelBase
+    {
+        private double _price;
+        private int _nightsCount;
+        private double _discount;
+        private double _total;
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _price = value;
+                Notify(nameof(Price));
+                _total = Price * NightsCount * (1 - Discount / 100);
+                Notify(nameof(Total));
+            }
+        }
+        public int NightsCount
+        {
+            get { return _nightsCount; }
+            set
+            {
+                if (value <= 0) throw new ArgumentException();
+                _nightsCount = value;
+                Notify(nameof(NightsCount));
+                _total = Price * NightsCount * (1 - Discount / 100);
+                Notify(nameof(Total));
+            }
+        }
+        public double Discount
+        {
+            get { return _discount; }
+            set
+            {
+                if (value < -100 || value > 100) throw new ArgumentException();
+                _discount = value;
+                Notify(nameof(Discount));
+                _total = Price * NightsCount * (1 - Discount / 100);
+                Notify(nameof(Total));
+            }
+        }
+        public double Total
+        {
+            get { return _total; }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _total = value;
+                Notify(nameof(Total));
+                _discount = Math.Abs((Total / (Price * NightsCount) - 1) * 100);
+                Notify(nameof(Discount));
+            }
+        }
+    }
+}
---
+++ 12_Theme_Data_Integrity/PocketGoogle.csproj/Indexer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PocketGoogle
+{
+    public class Word
+    {
+        public string Value;
+        public int Position;
+        public Word(string value, int position)
+        {
+            Value = value;
+            Position = position;
+        }
+    }
+
+    public class IndexDataStructure : Dictionary<string, Dictionary<int, List<int>>>
+    {
+        public void AddData(int id, Word word)
+        {
+            if (ContainsKey(word.Value))
+                if (this[word.Value].ContainsKey(id))
+                    this[word.Value][id].Add(word.Position);
+                else
+                    this[word.Value].Add(id, new List<int>() { word.Position });
+            else
+                Add(word.Value, new Dictionary<int, List<int>> { { id, new List<int> { word.Position } } });
+        }
+
+        public List<int> ReturnIds(string word)
+        {
+            var allIds = new List<int>();
+            if (ContainsKey(word))
+                foreach (var id in this[word].Keys)
+                    allIds.Add(id);
+            return allIds;
+        }
+
+        public void DeleteText(int textId)
+        {
+            foreach (var word in Keys)
+                if (this[word].ContainsKey(textId)) this[word].Remove(textId);
+        }
+
+        public List<int> FindWordPositionsInText(int textId, string word)
+        {
+            var allWordPositionsInText = new List<int>();
+            if (ContainsKey(word) && this[word].ContainsKey(textId))
+                allWordPositionsInText = this[word][textId].ToList();
+            return allWordPositionsInText;
+        }
+    }
+
+    public class TextParser
+    {
+        public List<Word> Parse(string documentText)
+        {
+            string wordPattern = @"[\p{L}|']+";
+            var wordsFromText = new List<Word>();
+            foreach (Match match in Regex.Matches(documentText, wordPattern, RegexOptions.Multiline))
+                wordsFromText.Add(new Word(match.Value, match.Index));
+            return wordsFromText;
+        }
+    }
+
+    public class Indexer : IIndexer
+    {
+        private IndexDataStructure statistics;
+        public Indexer()
+        {
+            statistics = new IndexDataStructure();
+        }
+
+        public void Add(int id, string documentText)
+        {
+            var textParser = new TextParser();
+            var allWordsFromText = textParser.Parse(documentText);
+            foreach (var word in allWordsFromText)
+                statistics.AddData(id, word);
+        }
+
+        public List<int> GetIds(string word)
+        {
+            return statistics.ReturnIds(word);
+        }
+
+        public List<int> GetPositions(int id, string word)
+        {
+            return statistics.FindWordPositionsInText(id, word);
+        }
+
+        public void Remove(int id)
+        {
+            statistics.DeleteText(id);
+        }
+    }
+}
